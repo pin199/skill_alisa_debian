f18b24150f0343d0ca18e0444aa4483d6f0848a1
pin199 <kirill-borisov-199@mail.ru>
2021-09-26 20:34:36 +0500

    ver 2.3

--- Parse/test1.py
+++ Parse/test1.py
@@ -93,7 +93,7 @@ content = http_req.request(url, method = "GET",headers = {'Host': 'yandex.ru',
         'Sec-Ch-Ua': '" Not A;Brand";v="99", "Chromium";v="92"',
         'Sec-Ch-Ua-Mobile': '?0',
         'Upgrade-Insecure-Requests': '1',
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gec',
+        'User-Agent': 'Safari/537.36 AppleWebKit/537.36 (KHTML, like Gec',
         'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image',
         'Sec-Fetch-Site': 'none',
         'Sec-Fetch-Mode': 'navigate',
@@ -102,9 +102,63 @@ content = http_req.request(url, method = "GET",headers = {'Host': 'yandex.ru',
         'Accept-Encoding': 'gzip, deflate',
         'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
         'Connection': 'close'})[1]
+soup_capcha = BeautifulSoup(content, 'html.parser')
+boxes_capcha = soup_capcha.find('div', class_='CheckboxCaptcha')
+boxes_capcha_t = boxes_capcha.find('form').attrs['action']
+boxes_capcha = boxes_capcha_t
+print("==============================")
+print(boxes_capcha)
+print("==============================")
+
+print("==============================")
+ 
+url_capcha = 'https://yandex.ru'+ boxes_capcha
+http_req_capcha = httplib2.Http()
+capcha_content = http_req_capcha.request(url_capcha,method = "GET",headers= {
+	'Host': 'yandex.ru',
+	'Cookie': 'mda=0; yandex_gid=56; yandexuid=7087684401630933978; yuidss=7087684401630933978; is_gdpr=0; is_gdpr_b=CLPnPBCARCgC; font_loaded=YSv1; ymex=1946294093.yrts.1630934093; _ym_uid=1630934123448513266; my=YwA=; _ym_d=1631103399; i=QoWLSC0mIWmloSGIeHN3MUlYbIAkedOneZ8Cjq3IdMeLq7VKZU4zxYTSJIlEcHLa0B2ho62CGKIf692aEX56Xcz3jtg=; spravka=dD0xNjMyNTc0ODQ1O2k9NzcuMjIyLjEwNy44O0Q9RjVCMEM2QkVEQzc1Rjg1NDc4MTA2RkQ2MDkwMUI0NTJGM0MwMThEN0ZDQjM1QTJGQzkzQzVFOTY3RkFDOTk1Q0U0NDA7dT0xNjMyNTc0ODQ1MTg1MjE0NDUxO2g9NmU4NjBkZDM3OGE3NmQ3MDZlOGIxZGJkZDA2NDYyZmQ=; yabs-frequency=/5/0000000000000000/bUDqs4l_UKpqGa3QagJCGnBgSlH28000/; yp=1633525979.ygu.1#1646702097.szm.1:1440x900:911x707#1633781776.csc.1#1664110856.p_sw.1632574855#1633179014.mcv.0#1633179014.mct.null#1633179014.mcl.; _yasc=jw6emxxhLoSlYCEWvt+1qnuATGtUWNj0nT5PwvWWPwQ/OgB9LjKpjw==',
+	'Sec-Ch-Ua': '"Chromium";v="93", " Not;A Brand";v="99"',
+	'Sec-Ch-Ua-Mobile': '?0',
+	'Sec-Ch-Ua-Platform': '"macOS"',
+	'Upgrade-Insecure-Requests': '1',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Sec-Fetch-Site': 'none',
+	'Sec-Fetch-Mode': 'navigate',
+	'Sec-Fetch-User': '?1',
+	'Sec-Fetch-Dest': 'document',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Connection': 'close'
+})[1]
+
+url_check_capcha = '/checkcaptcha?key=00Aa8x4LReWmhyTKSD4QzlWn6ck3qzVC_3%2F1632662498%2Ffa57d6f4db4c9a337822795ceba5cf83_07024daa9f4107e6e920a144bccb7b43&retpath=https%3A%2F%2Fyandex.ru%2Fsearch%3Flr%3D56%26text%3D%25D1%2581%25D0%25BC%25D0%25B5%25D1%2588%25D0%25B0%25D1%2580%25D0%25B8%25D0%25BA%25D0%25B8_f7638bef21a6e08dd809d7942c1c1c86&u=9c926652-b4d58985-9529d41f-a2027be3&rep=%D0%BF%D0%BE%D0%BA%D0%B8%D0%B4%D0%B0%D1%8E%20%D0%BF%D0%BE%D1%81%D0%B5%D1%82%D0%B8%D1%82%D0%B5%D0%BB%D0%B5'
+
+url_capcha_main = 'https://yandex.ru'+ url_check_capcha
+http_req_capcha_main = httplib2.Http()
+capcha_main_content = http_req_capcha_main.request(url_capcha_main, method = "POST", headers = {
+	'Host': 'yandex.ru',
+	'Content-Length': '1511',
+	'Cache-Control': 'max-age=0',
+	'Upgrade-Insecure-Requests': '1',
+	'Origin': 'http://yandex.ru',
+	'Content-Type': 'application/x-www-form-urlencoded',
+	'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/93.0.4577.82 Safari/537.36',
+	'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+	'Referer': 'http://yandex.ru/showcaptcha?retpath=https%3A//yandex.ru/search%3Flr%3D56%26text%3D%25D1%2581%25D0%25BC%25D0%25B5%25D1%2588%25D0%25B0%25D1%2580%25D0%25B8%25D0%25BA%25D0%25B8_f7638bef21a6e08dd809d7942c1c1c86&t=3/1632662498/fa57d6f4db4c9a337822795ceba5cf83&u=9c926652-b4d58985-9529d41f-a2027be3&s=5390fb6d8acdb4feccf3548aef9b7c4b',
+	'Accept-Encoding': 'gzip, deflate',
+	'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+	'Cookie': 'mda=0; font_loaded=YSv1; spravka=dD0xNjMyNTc0ODQ1O2k9NzcuMjIyLjEwNy44O0Q9RjVCMEM2QkVEQzc1Rjg1NDc4MTA2RkQ2MDkwMUI0NTJGM0MwMThEN0ZDQjM1QTJGQzkzQzVFOTY3RkFDOTk1Q0U0NDA7dT0xNjMyNTc0ODQ1MTg1MjE0NDUxO2g9NmU4NjBkZDM3OGE3NmQ3MDZlOGIxZGJkZDA2NDYyZmQ=; yp=1633525979.ygu.1#1646702097.szm.1:1440x900:911x707#1633781776.csc.1#1664110856.p_sw.1632574855#1633179014.mcv.0#1633179014.mct.null#1633179014.mcl.; gdpr=0; _ym_visorc=b',
+	'Connection': 'close'
+})[0]
 
 print(content.decode())
-
+print('=============================================')
+print(capcha_content.decode())
+print('=============================================')
+print('1')
+print(capcha_main_content.status)
+print(capcha_main_content['location'])
 #headers = req_search_yandex_without_capcha()
 #res_s = requests.get(url, headers = headers)
 
